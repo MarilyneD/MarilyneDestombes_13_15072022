4a3f028a0de510502d693f7b99201b6062f7c90b
MarilyneD <marilynedestombes@gmail.com>
2022-09-03 20:37:23 +0200

    initialize project using Create React App

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Footer from "./components/Footer";
-import MainNav from "./components/MainNav";
+import Header from "./components/Header";
 import Home from "./pages/Home";
 import Notfound from "./pages/Notfound";
-import SignIn from "./pages/SignIn";
-import Profile from "./pages/Profile";
-import EditName from "./pages/EditName";
-import SignUp from "./pages/SignUp";
+import EmployeesList from "./pages/EmployeesList";
+import CreateEmployee from "./pages/CreateEmployee";
+import VerticalNav from "./components/VerticalNav";
 
 function App() {
   return (
     <BrowserRouter>
-      <MainNav />
+      <VerticalNav />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/signin" element={<SignIn />} />
-        <Route path="/signup" element={<SignUp />} />
-        <Route path="/profile" element={<Profile />} />
-        <Route path="/editname" element={<EditName />} />
+        <Route path="/employeeslist" element={<EmployeesList />} />
+        <Route path="/createemployee" element={<CreateEmployee />} />
         <Route path="*" element={<Notfound />} />
       </Routes>
       <Footer />
